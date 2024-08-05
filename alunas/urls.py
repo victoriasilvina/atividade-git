a0dd712c8956f9287c75bf1ee2cf3dcaca67ca30
victoriasilvina <victoriasilvina36@gmail.com>
2024-08-04 22:33:58 -0300

    vic url

--- alunas/urls.py
+++ alunas/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from alunas.views import index
+from alunas.views import index, victoria
 
 urlpatterns = [
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('victoria', victoria, name="victoria")
 ]
--- alunas/views.py
+++ alunas/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'alunas/index.html')
+
+def victoria(request):
+    return render(request, 'alunas/victoria.html')
